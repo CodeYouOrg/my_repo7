f40580ff7e3b6198c47aee759520301f1ab50165
dmorton714 <102087408+dmorton714@users.noreply.github.com>
2024-08-07 10:04:42 -0400

    done

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("What is your name?");
+        string Name = Console.ReadLine();
+        Console.WriteLine("Hello, " + Name + "!")
+    }
+}
